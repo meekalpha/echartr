9f0a61c29cffd82d845c29f423e1212f14ece2a0
meekalpha <ashjw91@gmail.com>
2025-04-22 13:49:07 +0000

    Deploying to gh-pages from @ meekalpha/echartr@3ebb2f5ada05f5acd79a4f1c21ba93bfe8a3df98 🚀

---
+++ articles/echartr_files/echartr-binding-0.0.0.9000/echartr.js
@@ -0,0 +1,59 @@
+HTMLWidgets.widget({
+  name: 'echartr',
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    var chart;
+    var initialised = false;
+
+    return {
+
+      renderValue: function(x) {
+        if (!initialised) {
+
+          initialised = true;
+
+          if (HTMLWidgets.shinyMode) {
+            Shiny.addCustomMessageHandler("__echartr__".concat(el.id), x => {
+              update_echartr(chart, x)
+            });
+          }
+
+        }
+        chart = echarts.init(el);
+        chart.setOption(x.option);
+      },
+
+      resize: function(width, height) {
+      },
+
+    };
+  }
+});
+
+function update_echartr(chart, event) {
+  if (event.option !== null) {
+    chart.setOption(event.option);
+  }
+  if (event.on !== null) {
+    event.on.forEach(x => {
+      // TODO: More concise way?
+      console.log(eval(x.handler));
+      if (Object.hasOwn(x, "query")) {
+        chart.on(x.eventName, x.query, eval(x.handler), context = chart);
+      } else {
+        chart.on(x.eventName, eval(x.handler), context = chart);
+      }
+    });
+  }
+  if (event.off !== null) {
+    event.off.forEach(x => {
+      chart.off(x.eventName);
+    });
+  }
+}
+
+
+
+
